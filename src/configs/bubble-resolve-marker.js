3ec300911b5f286dbfbec744121d26c0336c41ab
Simon Porritt <simon@jsplumbtoolkit.com>
2026-09-21 13:09:34 +1000

    remove namespace from svg marker templates

--- src/configs/bubble-resolve-marker.js
+++ src/configs/bubble-resolve-marker.js
@@ -11,15 +11,15 @@ export default {
             resolveMarker:(p) => {
                 const id = `clip-${p.name.replace(/\s+/g, '-').toLowerCase()}`;
                 const imageUrl = `https://static.visuallyjs.com/img/flags/1x1/${p.name.toLowerCase()}.svg`;
-                return `<svg:svg viewBox="0 0 100 100">
-                                <svg:defs>
-                                    <svg:clipPath id="${id}">
-                                        <svg:circle cx="50" cy="50" r="50" />
-                                    </svg:clipPath>
-                                </svg:defs>
-                                <svg:image href="${imageUrl}" x="0" y="0" width="100" height="100" preserveAspectRatio="xMidYMid slice" clip-path="url(#${id})"/>
-                                <svg:circle cx="50" cy="50" r="48" fill="none" stroke="#ccc" stroke-width="2" />
-                            </svg:svg>`;
+                return `<svg viewBox="0 0 100 100">
+                                <defs>
+                                    <clipPath id="${id}">
+                                        <circle cx="50" cy="50" r="50" />
+                                    </clipPath>
+                                </defs>
+                                <image href="${imageUrl}" x="0" y="0" width="100" height="100" preserveAspectRatio="xMidYMid slice" clip-path="url(#${id})"/>
+                                <circle cx="50" cy="50" r="48" fill="none" stroke="#ccc" stroke-width="2" />
+                            </svg>`;
             }
         }
     ],
--- src/configs/scatter-custom-svg-marker.js
+++ src/configs/scatter-custom-svg-marker.js
@@ -1,5 +1,5 @@
 /**
- * Scatter chart with a custom SVG marker. The SVG is provided inline, using the VisuallyJs template format in which svg elements must be prefixed with the `svg:` namespace.
+ * Scatter chart with a custom SVG marker. The SVG is provided inline, using the VisuallyJs template format in which svg elements must be prefixed with the `` namespace.
  */
 export default {
   title:{
@@ -10,12 +10,12 @@ export default {
     {
       xAxisField:"sugar",
       yAxisField:"fat",
-      marker:`<svg:svg width="18" height="18" viewBox="0 0 6 6">
-  <svg:circle cx="3" cy="3" r="2.5" fill="yellow" stroke="black" stroke-width="0.2" />
-  <svg:circle cx="2" cy="2.2" r="0.3" stroke="black" fill="yellow"/>
-  <svg:circle cx="4" cy="2.2" r="0.3" stroke="black" fill="yellow" />
-  <svg:path d="M 1.8 3.8 Q 3 5 4.2 3.8" fill="none" stroke="black" stroke-width="0.3" stroke-linecap="round" />
-</svg:svg>`
+      marker:`<svg width="18" height="18" viewBox="0 0 6 6">
+  <circle cx="3" cy="3" r="2.5" fill="yellow" stroke="black" stroke-width="0.2" />
+  <circle cx="2" cy="2.2" r="0.3" stroke="black" fill="yellow"/>
+  <circle cx="4" cy="2.2" r="0.3" stroke="black" fill="yellow" />
+  <path d="M 1.8 3.8 Q 3 5 4.2 3.8" fill="none" stroke="black" stroke-width="0.3" stroke-linecap="round" />
+</svg>`
     }
   ]
 }
